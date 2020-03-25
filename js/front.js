6f5d3b866f133f6df052ba0d1b2b05a00e1a4993
kangseongjoo <ksj_design@naver.com>
2020-03-25 21:05:50 +0900

    수정

--- js/front.js
+++ js/front.js
@@ -5,30 +5,37 @@
 
 
 var inputTxt = null,
-    btn = null;
-
-function init(){
+    btn = null,
+    tbl;
+    
     inputTxt = document.getElementById('input-txt');
     btn = document.getElementById('btn');
+    btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+    var val = inputTxt.value;
+
+    if (val === '') {
+        alert('입력하세요')
+        return;
+    }
+
+    
+    
+    localStorage.setItem(val, val)
+
+
+    addTbl(val)
+
+})
+
+function addTbl(val) {
+    document.createElement()
 }
 
 
 
 
 
-btn.addEventListener('click', function(){
-
-    
-
-})
 
 
-
-
-
-
-
-
-
-
-init()
