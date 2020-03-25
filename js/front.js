b91efb80cbcef65ad156cfa469f1216c3247732d
kangseongjoo <ksj_design@naver.com>
2020-03-25 17:22:04 +0900

    초기 스크립트 세팅

---
+++ js/front.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+var inputTxt = document.getElementById('input-txt');
+var btn = document.getElementById('btn');
+
+
+
+btn.addEventListener('click', function(){
+
+    localStorage.setItem()
+
+})
+
+
+
+
+
+
+
+
+
+
