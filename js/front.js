6bbecd1c861b7375185fdce26b4bfc076181bbb4
kangseongjoo <ksj_design@naver.com>
2020-03-27 10:53:04 +0900

    빠른 속도에서 감소 알고리즘 로직 작성

--- js/front.js
+++ js/front.js
@@ -3,11 +3,15 @@ var input = null,
     btnAdd = null,
     btnAllDel = null,
     btnDel = null,
-    tbl = null;
+    btnSelect = null,
+    tbl = null,
+    selectSet = null,
+    selectNum = 0;
 
 input = document.getElementById('input-txt');
 btnAdd = document.getElementById('btn-add');
 btnAllDel = document.getElementById('btn-all-del');
+btnSelect = document.getElementById('btn-select');
 tbl = document.getElementById('tbl');
 
 btnAdd.addEventListener('click', function () {
@@ -16,7 +20,7 @@ btnAdd.addEventListener('click', function () {
         alert('입력 X')
         return;
     }
-    for (var i = 0;i < localStorage.length; i++) {
+    for (var i = 0; i < localStorage.length; i++) {
         if (localStorage.getItem(val)) {
             alert('중복')
             return;
@@ -27,12 +31,12 @@ btnAdd.addEventListener('click', function () {
     input.value = '';
 })
 
-btnAllDel.addEventListener('click', function(e){
+btnAllDel.addEventListener('click', function (e) {
     localStorage.clear()
     delAllTbl()
 })
 
-tbl.addEventListener('click', function(e){
+tbl.addEventListener('click', function (e) {
     var trIdx = 0,
         trTarget = null;
     if (e.target.classList[0] === 'btn-del') {
@@ -41,6 +45,9 @@ tbl.addEventListener('click', function(e){
         tbl.removeChild(trTarget);
         localStorage.removeItem(e.target.previousSibling.data)
     }
+})
+btnSelect.addEventListener('click', function (e) {
+    selectPoint()
 })
 
 function addTbl(val) {
@@ -57,11 +64,40 @@ function delAllTbl() {
     }
 }
 
-function getStorage(){
+function getStorage() {
     for (var i = 0; i < localStorage.length; i++) {
         addTbl(localStorage.key(i))
     }
 }
+
+function selectPoint() {
+    if (selectNum === 0) {
+        selectStart(10, 25)
+    } else if (selectNum === 10) {
+        selectStart(15, 50)
+    } else if (selectNum === 15) {
+        selectStart(20, 100)
+    } else if (selectNum === 20) {
+        selectStart(25, 250)
+    } else if (selectNum === 30) {
+        selectStart(33, 500)
+    } else if (selectNum === 33) {
+        selectStart(35, 1000)
+    }
+}
+
+function selectStart(point, spped) {
+    selectSet = setInterval(function () {
+        selectNum++;
+        if (selectNum === point) {
+            clearInterval(selectSet)
+            selectPoint()
+        }
+        console.log(selectNum)
+    }, spped)
+}
+
+
 
 getStorage()
 
